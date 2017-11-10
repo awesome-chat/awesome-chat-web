1f0c88a3bd9b79c4491aaae665d06ff7d64b3341
wengwengweng <593110501@qq.com>
2017-11-10 23:48:47 +0800

    add server code

--- server/main.js
+++ server/main.js
@@ -1,2 +1,19 @@
-// new server
-require('@config/devserver')
+const Koa = require('koa');
+const config = require('../config/server');
+const app = new Koa();
+
+// response
+app.use(async (ctx, next) => {
+  ctx.body = "Hello world";
+});
+app.listen(config.port);
+
+// delay its appearance to make sure user can notice it..
+setTimeout(() => {
+  console.log('-------------------------------');
+  console.log(app)
+  console.log(`app server is listening on ${config.port}`);
+  console.log('-------------------------------');
+}, 1000);
+
+module.exports = app;
