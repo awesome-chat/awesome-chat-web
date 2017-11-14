ccccf182f043e36e03af3a9461717f7be87df69c
wengrunyu <wengrunyu@meituan.com>
2017-11-14 10:26:36 +0800

    update

--- server/main.js
+++ server/main.js
@@ -1,22 +1,21 @@
 const Koa = require('koa');
 const config = require('../config/default');
 const fs = require('fs');
-var router = require('./router');
 const app = new Koa();
 
 require('../config/devserver')()
 
 // https://github.com/glenjamin/ultimate-hot-reloading-example/issues/17
-// this is important!
-app.use(function(req, res, next) {
-  require('./router').routes()(req, res, next);
+app.use(function(ctx, next) {
+  require('./router').routes()(ctx, next);
 });
 
 fs.watch(require.resolve('./router'), function() {
-  updateRequire('./router')
+  serverHotReload('./router')
 })
 
-function updateRequire(module){
+// router热更新
+function serverHotReload(module){
   console.log(`change file: ${module}`)
   Object.keys(require.cache).forEach(function(id) {
     if (/[\/\\]router[\/\\]/.test(id)) delete require.cache[id];
--- server/router/index.js
+++ server/router/index.js
@@ -1,8 +1,11 @@
 const Router = require('koa-router');
 const router = new Router();
+const fs = require('fs');
+const path = require('path');
 
 router.get('/', async (ctx, next) => {
-  ctx.body = "Hello world123";
+  const filePath = path.resolve('./server/template/index.html')
+  ctx.body = fs.readFileSync(filePath,'utf-8');
 });
 
 module.exports = router;
