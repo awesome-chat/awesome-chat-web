f1211726bbb7f0fa6ae2646bf5b6adbc67eb4862
wengwengweng <593110501@qq.com>
2018-01-12 09:44:04 +0800

    add pord config

---
+++ config/pord.webpack.config.js
@@ -0,0 +1,47 @@
+const webpack = require('webpack');
+const path = require('path');
+const { alias } = require('./default');
+
+const baseConfig = {
+  context: alias['@root'],
+  entry: {
+    main:[path.resolve(__dirname, '../client/index/index.jsx'),'webpack-hot-middleware/client?reload=true'],
+  },
+  module: {
+    loaders: [{
+      test: /\.jsx?$/,
+      include: [alias['@client']],
+      loader: 'babel-loader',
+      query: {
+        presets: ['es2015', 'stage-0', 'react']
+      }
+    },{
+      test: /\.css$/,
+      use: [
+        {
+          loader: 'style-loader',
+        },
+        {
+          loader: 'css-loader',
+          options: {
+            importLoaders: 1,
+          }
+        },
+        {
+          loader: 'postcss-loader'
+        }
+      ]
+    }]
+  },
+  output: {
+    path: alias['@build'],
+    publicPath: '/build/',
+    filename: 'index.js',
+  },
+  devtool: 'inline-source-map',
+  plugins: [
+    new webpack.optimize.UglifyJsPlugin(),
+  ],
+};
+
+module.exports = baseConfig;
